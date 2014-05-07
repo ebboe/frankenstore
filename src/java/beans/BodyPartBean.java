5da52d1f5aaf552357da6ce856dc814c49bf7040
Farshid Besharati <farshidbt@gmail.com>
2014-05-07 14:44:07 +0200

    * HEJ HEJ

--- src/java/beans/BodyPartBean.java
+++ src/java/beans/BodyPartBean.java
@@ -19,8 +19,44 @@ public class BodyPartBean {
     }
     
     public int getBodyPartId() {
-    
+        return bodyPartId;
     }
-}
     
+    public void setBodyPartId(int partId) {
+        bodyPartId = partId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String partName) {
+        name = partName;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(int newQuantity) {
+        quantity = newQuantity;
+    }
+    
+    public String getXml() {
+        StringBuilder xmlOut = new StringBuilder();
+        
+        xmlOut.append("<bodypart>");
+            xmlOut.append("<id>");
+                xmlOut.append(bodyPartId);
+            xmlOut.append("</id>");
+            xmlOut.append("<name><![CDATA[");
+                xmlOut.append(name);
+            xmlOut.append("]]</name>");
+            xmlOut.append("<quantity>");
+                xmlOut.append(quantity);
+            xmlOut.append("</quantity>");
+        xmlOut.append("</bodypart>");
+        
+        return xmlOut.toString();
+    }
 }
---
+++ src/java/beans/BodyPartListBean.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+
+/**
+ *
+ * @author farshid
+ */
+public class BodyPartListBean {
+    private Collection bodyPartList;
+    private String url;
+    
+    public BodyPartListBean() throws Exception {
+        this("jdbc:mysql://localhost/frankenstore?user=root&password=jip9olp9od9Cyop");
+    }
+    
+    public BodyPartListBean(String _url) throws Exception {
+        url = _url;
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        bodyPartList = new ArrayList();
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url);
+            
+            stmt = conn.createStatement();
+            String sql = "SELECT COMPONENT_ID, NAME, QUANTITY AS AUTHOR_NAME FROM COMPONENTS";
+            
+            rs = stmt.executeQuery(sql); 
+            
+            while (rs.next()) {
+                BodyPartBean partBean = new BodyPartBean();
+                
+                partBean.setBodyPartId(rs.getInt("COMPONENT_ID"));
+                partBean.setName(rs.getString("NAME"));
+                partBean.setQuantity(rs.getInt("QUANTITY"));
+                
+                bodyPartList.add(partBean);
+            }
+            
+        } catch (SQLException e) {
+            throw new Exception(e);
+        } finally {
+            try {
+                rs.close();
+            } catch (Exception e) {} 
+            
+            try {
+                stmt.close();
+            } catch (Exception e) {}
+            
+            try {
+                conn.close();
+            } catch (Exception e) {}
+        }
+    }
+    
+    java.util.Collection getBodyPartList() {
+        return bodyPartList;
+    }
+    
+    public String getXml() {
+        BodyPartBean partBean;
+        Iterator iter = bodyPartList.iterator();
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("<bodypartlist>");
+            while (iter.hasNext()) {
+                partBean = (BodyPartBean)iter.next();
+                sb.append(partBean.getXml());
+            }
+        sb.append("</bodypartlist>");
+        
+        return sb.toString();
+    }
+    
+}
